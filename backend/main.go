2242f49be734f64efedb7cad1218971af11f634c
web3santa <usefulstory35@gmail.com>
2024-03-30 23:11:06 +0900

    Add read methods

--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,48 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"santa/websocket-react/pkg/websockets"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func serveWs(pool *websockets.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("websocket endpoint reached")
+
+	conn, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		fmt.Fprintf(w, "%+v\n", err)
+	}
+
+	client := &websockets.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+	pool.Register <- client
+	client.Read()
+
+}
+
+func setupRoutes() {
+	pool := websockets.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
+	})
+
+}
+
+func main() {
+	fmt.Println("full stack cha project")
+	setupRoutes()
+	http.ListenAndServe(":9000", nil)
+}
---
+++ backend/pkg/websockets/pool.go
@@ -0,0 +1,30 @@
+package websockets
+
+import "fmt"
+
+type Pool struct {
+	Register   chan *Client
+	Unregister chan *Client
+	Clients    map[*Client]bool
+	Broadcast  chan Message
+}
+
+func NewPool() *Pool {
+	return &Pool{
+		Register:   make(chan *Client),
+		Unregister: make(chan *Client),
+		Clients:    make(map[*Client]bool),
+		Broadcast:  make(chan Message),
+	}
+}
+
+func (pool *Pool) Start() {
+	for {
+		select {
+		case client := <-pool.Register:
+			pool.Clients[client] = true
+			fmt.Println("size of connection pool:", len(pool.Clients))
+
+		}
+	}
+}
